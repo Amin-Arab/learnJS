d380a9f956cf08b506b69bf645b800e206b01f00
amin arab <aminarab0910@gamil.com>
2023-11-06 15:53:04 +0330

    this is my first bmi calc

---
+++ insex.js
@@ -0,0 +1,16 @@
+// mark 
+const Markkg = 78 ; 
+const markHighe = 1.69 ; 
+
+//john 
+const johnkg = 92 ;
+const johnhighe = 1.96 ; 
+
+//bmi calculator
+const makbmi = Markkg / (markHighe * markHighe);
+console.log(makbmi);
+const johnbmi = (johnkg) / (markHighe ** 2);
+console.log(johnbmi);
+//Comparison
+const calc = makbmi > johnbmi ; 
+console.log(calc);
